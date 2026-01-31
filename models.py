9196d2f9a09c9e8c69cb7244d5b0de3b9d9ab30e
Carlos Henrique <carlinhosbackend@gmail.com>
2026-01-31 12:39:16 -0300

    Finish models and add config database

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import ChoiceType
+
 
 db = create_engine("sqlite:///banco.db")
 base = declarative_base()
@@ -19,3 +21,39 @@ class Usuario(base):
         self.senha = senha
         self.ativo = ativo
         self.admin = admin
+
+class Pedido(base):
+    __tablename__ = "pedidos"
+
+    STATUS_PEDIDOS = (
+        ("pendente", "Pendente"),
+        ("cancelado", "Cancelado"),
+        ("finalizado", "Finalizado"),
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS), nullable=False)
+    usuario_id = Column("usuario_id", Integer, nullable=False, ForeignKey("usuarios.id"))
+    preco_total = Column("preco_total", Float, nullable=False)
+
+    def __init__(self, status = "pendente", usuario_id, preco_total = 0.0):
+        self.status = status
+        self.usuario_id = usuario_id
+        self.preco_total = preco_total
+    
+
+class ItemPedido(base):
+    __tablename__ = "item_pedido"
+    id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
+    quantidade = Column("quantidade", Integer, nullable=False)
+    sabor = Column("sabor", String, nullable=False)
+    tamanho = Column("tamanho",String)
+    preco = Column("preco", Float, nullable=False)
+    pedido_id = Column("pedido_id", Integer, nullable=False, ForeignKey("pedidos.id"))
+
+    def __init__(self, quantidade, sabor, tamanho, preco, pedido_id):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco = preco
+        self.pedido_id = pedido_id
